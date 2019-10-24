ee6d2175f55f971ee512a13cd058b513c0acd42b
smathew18 <smathew@unomaha.edu>
2019-10-24 18:09:45 -0500

    backend creation

--- ORC/admin.py
+++ ORC/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class PropertyLocationList(admin.ModelAdmin):
+    list_display = ( 'propertyId', 'propertyName' )
+    list_filter = ( 'propertyId', 'propertyName')
+    search_fields = ('propertyId', )
+    ordering = ['propertyId']
+
+class MaintenanceWorkList(admin.ModelAdmin):
+    list_display = ( 'mwId', 'mwDescription','woId' )
+    list_filter = ( 'mwId', 'mwDescription','woId')
+    search_fields = ('mwId', )
+    ordering = ['mwId']
+
+class WorkorderList(admin.ModelAdmin):
+    list_display = ( 'woId', 'woDescription', 'woPriority' )
+    list_filter = ( 'woId', 'woDescription', 'woPriority')
+    search_fields = ('woId', )
+    ordering = ['woId']
+
+class EquipmentList(admin.ModelAdmin):
+    list_display = ( 'equipId', 'equipName' )
+    list_filter = ( 'equipId', 'equipName')
+    search_fields = ('equipId', )
+    ordering = ['equipId']
+
+admin.site.register(PropertyLocation, PropertyLocationList)
+admin.site.register(MaintenanceWork, MaintenanceWorkList)
+admin.site.register(Workorder, WorkorderList)
+admin.site.register(Equipment, EquipmentList)
